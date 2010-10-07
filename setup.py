b41403e59b03409f536b98b2e09386ba39fdb38b
Hanno Schlichting <hanno@hannosch.eu>
2010-10-07 10:07:58 +0000

    Correct version, b1 was already released
    svn path=/plone.app.caching/trunk/; revision=40545

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0b1'
+version = '1.0b2'
 
 setup(name='plone.app.caching',
       version=version,
       description="Plone UI and default rules for plone.caching/z3c.caching",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='plone caching',
       author='Plone Foundation',
