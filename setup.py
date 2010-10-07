84aa8af6bb4f91d031450928613ed8addca0b4de
Hanno Schlichting <hanno@hannosch.eu>
2010-10-07 10:13:19 +0000

    Update distribution metadata to current best practice.
    svn path=/plone.app.caching/trunk/; revision=40549

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-import os
 
 version = '1.0b2'
 
@@ -7,16 +6,20 @@ setup(name='plone.app.caching',
       version=version,
       description="Plone UI and default rules for plone.caching/z3c.caching",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+                       open("CHANGES.txt").read(),
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
         ],
       keywords='plone caching',
       author='Plone Foundation',
       author_email='plone-developers@lists.sourceforge.net',
       url='http://pypi.python.org/pypi/plone.app.caching',
-      license='GPL',
+      license='GPL version 2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['plone', 'plone.app'],
       include_package_data=True,
