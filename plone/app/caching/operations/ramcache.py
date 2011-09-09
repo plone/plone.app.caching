04fab826e01e82eaaf634259bf487a9346f87887
Godefroid Chapelle <gotcha@bubblenet.be>
2011-09-09 14:32:27 +0000

    When there was an error like Unauthorized, 200 status and empty body would be cached in RAMCache instead of not caching anything. This is due to a bug with Zope 2.13 publication events : response.status is not set when IPubBeforeAbort is notified. Fixed by using error_status stored on request by plone.transformchain.
    svn path=/plone.app.caching/trunk/; revision=51959

--- plone/app/caching/operations/ramcache.py
+++ plone/app/caching/operations/ramcache.py
@@ -2,6 +2,7 @@ from zope.interface import implements
 from zope.interface import Interface
 
 from zope.component import adapts
+from zope.annotation.interfaces import IAnnotations
 
 from plone.transformchain.interfaces import ITransform
 
@@ -13,50 +14,47 @@ GLOBAL_KEY = 'plone.app.caching.operations.ramcache'
 
 class Store(object):
     """Transform chain element which actually saves the page in RAM.
-    
+
     This is registered for the ``IRAMCached`` request marker, which is set by
     the ``cacheInRAM()`` helper method. Thus, the transform is only used if
     the caching operation requested it.
     """
-    
+
     implements(ITransform)
     adapts(Interface, Interface)
-    
+
     order = 90000
-    
+
     def __init__(self, published, request):
         self.published = published
         self.request = request
-    
+
     def transformUnicode(self, result, encoding):
-        if not IRAMCached.providedBy(self.request):
+        if not IRAMCached.providedBy(self.request) or self.inError():
             return None
-        
-        status = self.request.response.getStatus()
-        if status != 200:
-            return None
-        
+
         storeResponseInRAMCache(self.request, self.request.response, result.encode(encoding))
         return None
-    
+
     def transformBytes(self, result, encoding):
-        if not IRAMCached.providedBy(self.request):
+        if not IRAMCached.providedBy(self.request) or self.inError():
             return None
-        
-        status = self.request.response.getStatus()
-        if status != 200:
-            return None
-        
+
         storeResponseInRAMCache(self.request, self.request.response, result)
         return None
-    
+
     def transformIterable(self, result, encoding):
-        if not IRAMCached.providedBy(self.request):
+        if not IRAMCached.providedBy(self.request) or self.inError():
             return None
-        
-        status = self.request.response.getStatus()
-        if status != 200:
-            return None
-        
+
         storeResponseInRAMCache(self.request, self.request.response,''.join(result))
         return None
+
+    def inError(self):
+        status = self.request.response.getStatus()
+        annotations = IAnnotations(self.request.response, None)
+        if annotations is None:
+            error_status = None
+        else:
+            error_status = annotations['error_status']
+        return status != 200 or error_status and error_status != 200
