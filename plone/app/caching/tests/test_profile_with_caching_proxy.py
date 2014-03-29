3fc4939fe1a08142472121080a007e935c85b838
Philip Bauer <bauer@starzel.de>
2014-03-29 18:13:46 +0100

    fix previously included snafu

--- plone/app/caching/tests/test_profile_with_caching_proxy.py
+++ plone/app/caching/tests/test_profile_with_caching_proxy.py
@@ -425,7 +425,7 @@ class TestProfileWithCaching(unittest.TestCase):
         # Request a skin image
         now = stable_now()
         browser = Browser(self.app)
-        browser.open(self.portal.absolute_url() + '/rss.stable_nowg')
+        browser.open(self.portal.absolute_url() + '/rss.png')
         self.assertEqual('plone.resource', browser.headers['X-Cache-Rule'])
         self.assertEqual('plone.app.caching.strongCaching', browser.headers['X-Cache-Operation'])
         # This should use cacheInBrowserAndProxy
@@ -440,7 +440,7 @@ class TestProfileWithCaching(unittest.TestCase):
         browser = Browser(self.app)
         browser.raiseHttpErrors = False
         browser.addHeader('If-Modified-Since', lastmodified)
-        browser.open(self.portal.absolute_url() + '/rss.stable_nowg')
+        browser.open(self.portal.absolute_url() + '/rss.png')
         self.assertEqual('plone.resource', browser.headers['X-Cache-Rule'])
         self.assertEqual('plone.app.caching.strongCaching', browser.headers['X-Cache-Operation'])
         # This should be a 304 response
