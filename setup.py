3bffa7292b9a8e9d5ef2f599bb776048019196f5
Maurits van Rees <maurits@vanrees.org>
2018-10-01 15:32:52 +0200

    Fixed PyPI links in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     keywords='plone caching',
     author='Plone Foundation',
     author_email='plone-developers@lists.sourceforge.net',
-    url='https://pypi.python.org/pypi/plone.app.caching',
+    url='https://pypi.org/project/plone.app.caching',
     license='GPL version 2',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['plone', 'plone.app'],
