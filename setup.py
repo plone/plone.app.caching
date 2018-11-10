04a79bf1a8ee2b476641dde016361d44171e29ac
Gil Forcada Codinachs <gil.gnome@gmail.com>
2018-11-10 21:56:37 +0100

    Update trove classifiers
    [ci-skip]

--- setup.py
+++ setup.py
@@ -22,6 +22,8 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords='plone caching',
     author='Plone Foundation',
